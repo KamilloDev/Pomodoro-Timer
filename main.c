e9bfccab861f76b02c4ef95bbe89ba4843f13122
Krystian Kamil Kowalczyk <krystiankowalczyk1204@gmail.com>
2026-04-21 10:10:44 +0200

    Implemented basic timer

--- main.c
+++ main.c
@@ -6,11 +6,23 @@
 int main() {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pomodoro Timer");
   SetTargetFPS(60);
+
+  const int TIME = 60;
   
   
   while (!WindowShouldClose()) {
-	BeginDrawing();
-	ClearBackground(WHITE);
+	double timeInMinutes = (TIME - (int)GetTime()) / 60;
+	double timeInSeconds = (TIME - (int)GetTime()) % 60;
+	int timeLeft = TIME - (int)GetTime();
+	
+    BeginDrawing();
+    	ClearBackground(RAYWHITE);
+		if (timeLeft > 0) {
+			DrawText(TextFormat("%.0f : %.0f", timeInMinutes, timeInSeconds), SCREEN_WIDTH / 2 - 60, SCREEN_HEIGHT / 2, 60, BLACK);
+		} else {
+			DrawText("TIME RUN OUT", SCREEN_WIDTH / 2 - 60, SCREEN_HEIGHT / 2, 60, RED);
+		}
+
     EndDrawing();
   }
 
